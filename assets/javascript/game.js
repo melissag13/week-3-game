3b8c3fa6cbec9eb9be2c06a789086e718c64c442
Michael Garcia <michaelgarcia@Melissa-Garcias-iMac.local>
2016-07-16 01:11:34 -0400

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,5 @@
 
 
-  var wordBank = [
-      "Michael Myers",
-      "Laurie Strode",
-      "Jamie Lloyd",
-      "Haddonfield, Illinois"
-  ];
-  
-  var words = wordBank[Math.floor(Math.random()*wordBank.length)];
-    console.log(words);
-
- 
   
 // sound 
 
@@ -19,3 +8,74 @@ audio.play();
 
 var audio = new Audio("audio/Happy_Halloween.mp3");
 audio.play();
+
+// var win = new Audio("audio/Evil_Laugh.mp3");
+// if(playerWon === true){
+//     win.play();
+// }
+
+// var lost = new Audio("audio/Scream.mp3");
+// if(playerLost === true){
+//     lost.play();
+// }
+
+ 
+     // Get elements
+  var showLives = document.getElementById("mylives");
+  
+    
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives ) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < wordBank.length    ; i++) {
+      if (wordBank.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+   }
+
+      
+  
+    
+  // Play
+
+   // play = function () {}
+         
+    
+    var geusses = [ ];
+    var lives = 10;
+
+  var wordBank = [
+      "Michael Myers",
+      "Laurie Strode",
+      "Jamie Lloyd",
+      "Haddonfield, Illinois"
+    ];
+  
+  function mywordBank(e) {
+  var words = wordBank[Math.floor(Math.random()*wordBank.length)];
+    console.log(words);
+
+  var choice = String.fromCharCode (e.keyCode).toUpperCase();
+   
+    
+  
+
+  }
+ // Reset
+
+  document.getElementById('reset').onclick = function() {
+    
+  }
+
+
+
+
+
+
+
+
+
